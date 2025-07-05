0edfef5bab98196cfd3dcad1d28b182e0d767b20
Saravanakumar Murugesan <sam1196486@gmail.com>
2025-07-05 12:47:05 +0530

    initial setup done

---
+++ gitlabagent.py
@@ -0,0 +1,6 @@
+import os
+
+from langchain.agents import AgentType, initialize_agent
+from langchain_community.agent_toolkits.gitlab.toolkit import GitLabToolkit
+from langchain_community.utilities.gitlab import GitLabAPIWrapper
+from langchain_openai import OpenAI
